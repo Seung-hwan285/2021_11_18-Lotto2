c90b79e0ad0079555e65aa383fad843aec1a3239
Seung-hwan285 <ghks285@naver.com>
2021-11-19 23:19:28 +0900

    ref: 주석수정 , 전체적으로 필요없는 코드 삭제

--- src/main/java/domain/WinnerGenerator.java
+++ src/main/java/domain/WinnerGenerator.java
@@ -2,7 +2,6 @@ package domain;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class WinnerGenerator {
 
@@ -11,55 +10,44 @@ public class WinnerGenerator {
      *   [] 입력받은 당첨 번호 split
      *      - null,isEmpty 예외
      *      - 구분자 분리 ","
-     *      - 문자열 -> 정수형 리스트
      *      - 숫자 6개 예외
-     *
-     *   [] 모든 예외처리 및 리스트 정수형으로 변환 -> checkTotalThrowInteger()
+
      */
 
-    private List<Integer> wiList=new ArrayList<>();
-
-
     public WinnerGenerator(String input){
-        checkTotalThrowInteger(input);
+
         checkNull(input);
         checkSeparator(input);
-    }
 
-    // [] 모든 예외처리 및 리스트 정수형으로 변환
-    public static void checkTotalThrowInteger(String input){
-            checkNull(input);
-            checkSeparator(input);
+        List<String> splitNumber=changeSplit(input);
 
-            //split 분리
-            List<String> changeSplit=changeSplit(input);
-            checkLengthSix(changeSplit);
-            // - 문자열 -> 정수형 리스트
-
-    }
-
-    // - 숫자 6개 예외
-    public static void checkLengthSix(List<String> changeSplit){
-
-        if(!(changeSplit.size() ==6)){
-            throw new IllegalArgumentException("숫자 6개 입력해주세요");
-        }
+        checkLengthSix(splitNumber);
     }
 
 
     // [] 입력받은 당첨 번호 split
     public static List<String> changeSplit(String input){
-        String inputSplit[] = input.split(",");
-        List<String> chagneList=new ArrayList<>();
-        for(int i=0; i<inputSplit.length; i++){
-            chagneList.add(inputSplit[i]);
+        String splitNum[]=input.split(",");
+        List<String> inputSplitList=new ArrayList<>();
+
+        for(int i=0; i<splitNum.length; i++){
+            inputSplitList.add(splitNum[i]);
         }
-        return chagneList;
+        return inputSplitList;
+
     }
 
-    //- null,isEmpty 예외
+
+    // 숫자 6개 예외
+    public static void checkLengthSix(List<String> inputSplitLitst){
+        if(!(inputSplitLitst.size()==6)){
+            throw new IllegalArgumentException("숫자 6개 입력해주세요");
+        }
+    }
+
+    // - null,isEmpty 예외
     public static void checkNull(String input){
-        if(input==null || input.trim().isEmpty()){
+        if(input==null || input.isEmpty()){
             throw new IllegalArgumentException("당첨 번호를 입력하지 않으셨습니다.");
         }
     }
